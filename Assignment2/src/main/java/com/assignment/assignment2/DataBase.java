d115530d5c6c8275c1c2f783ae719d3502e940ea
Anthony <anth19800@hotmail.com>
2022-04-04 23:27:32 -0400

    [Add] Stuff

---
+++ Assignment2/src/main/java/com/assignment/assignment2/DataBase.java
@@ -0,0 +1,16 @@
+package com.assignment.assignment2;
+
+import java.util.ArrayList;
+
+public class DataBase {
+    //first row is column names
+    public static ArrayList<ArrayList<String>> data;
+    
+    public static void AppendColumn(String name, ArrayList<String> inData){
+        data.get(0).add(name);
+        for (int i = 1; i < inData.size(); i++) {
+            data.get(i).set(data.get(i).size() - 1,inData.get(i - 1));
+        }
+    }
+
+}
